0850afd54e4bfafc9e2ce63f2f7fee99f5ea1753
x57309461 <x57309461@gmail.com>
2026-09-13 15:21:46 +0800

    Update due.js

--- functions/api/due.js
+++ functions/api/due.js
@@ -9,7 +9,7 @@ export async function onRequestGet(context) {
       WHERE wordbook_id = ? 
         AND (due IS NULL OR due <= ?)
       ORDER BY due ASC
-      LIMIT 1000`)  //重置本书进度1000个单词量
+      LIMIT 10000`)  //重置本书进度10000个单词量
     .bind(wordbookId, today)
     .all();
 
